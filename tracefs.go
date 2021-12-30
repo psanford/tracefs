6fdea938a23020531aab33db42cfbc0383927b36
Peter Sanford <psanford@sanford.io>
2021-12-29 16:29:05 -0800

    Add basic uprobe tracing support

--- tracefs.go
+++ tracefs.go
@@ -3,9 +3,11 @@ package tracefs
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type Instance struct {
@@ -56,6 +58,11 @@ func ListInstances() ([]Instance, error) {
 	return DefaultInstance.ChildInstances()
 }
 
+// Create a new child tracer instance. This only works when called on the root instance.
+func NewInstance(name string) (*Instance, error) {
+	return DefaultInstance.NewInstance(name)
+}
+
 type Tracer string
 
 const (
@@ -103,7 +110,7 @@ func (i *Instance) SetTracer(t Tracer) error {
 }
 
 // On returns true if tracing_on is set to 1.
-func (i Instance) On() (bool, error) {
+func (i *Instance) On() (bool, error) {
 	result, err := i.readFile(tracingOnPath)
 	if err != nil {
 		return false, err
@@ -119,13 +126,59 @@ func (i Instance) On() (bool, error) {
 }
 
 // Enable sets tracing_on to 1. If tracing is already enabled this is a no-op.
-func (i Instance) Enable() error {
+func (i *Instance) Enable() error {
 	return i.writeFile(tracingOnPath, []byte("1"))
 }
 
 // Disable sets tracing_on to 0. If tracing is already disabled this is a no-op.
-func (i Instance) Disable() error {
+func (i *Instance) Disable() error {
 	return i.writeFile(tracingOnPath, []byte("0"))
+}
+
+// Create a new child tracer instance. This only works when called on the root instance.
+func (i *Instance) NewInstance(name string) (*Instance, error) {
+	if !i.isRoot {
+		return nil, fmt.Errorf("must be called on a root instance")
+	}
+
+	childPath := filepath.Join(i.path, "instances", name)
+	err := os.Mkdir(childPath, 0777)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Instance{
+		path: childPath,
+		name: name,
+	}, nil
+}
+
+// Destory tracer instance. This does not work on the root instance
+func (i *Instance) Destroy() error {
+	if i.isRoot {
+		return fmt.Errorf("cannot destroy the root tracer instance")
+	}
+
+	return os.Remove(i.path)
+}
+
+func (i *Instance) AddUprobeEvent(e *UprobeEvent) error {
+	f, err := os.OpenFile(filepath.Join(i.path, "uprobe_events"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = fmt.Fprintln(f, e.Rule())
+	if err != nil {
+		return err
+	}
+
+	return f.Close()
+}
+
+func (i *Instance) TracePipe() (io.ReadCloser, error) {
+	return os.Open(filepath.Join(i.path, "trace_pipe"))
 }
 
 type UprobeEvent struct {
@@ -133,8 +186,50 @@ type UprobeEvent struct {
 	Group       string
 	Event       string
 	Path        string
-	Offset      string
+	Offset      uint64
 	FetchArgs   []FetchArg
+}
+
+func (e *UprobeEvent) Rule() string {
+	typ := "p"
+	if e.ReturnProbe {
+		typ = "r"
+	}
+
+	var builder strings.Builder
+
+	builder.Write([]byte(typ))
+	if e.Group != "" && e.Event != "" {
+		fmt.Fprintf(&builder, ":%s/%s", e.Group, e.Event)
+	} else if e.Event != "" {
+		fmt.Fprintf(&builder, ":%s", e.Event)
+	}
+
+	fmt.Fprintf(&builder, " %s:0x%016x", e.Path, e.Offset)
+
+	for _, arg := range e.FetchArgs {
+		fmt.Fprintf(&builder, " %s", arg.String())
+	}
+
+	return builder.String()
+}
+
+func (i *Instance) UprobeEnablePath(e *UprobeEvent) string {
+	if e.Group != "" && e.Event != "" {
+		return filepath.Join(i.path, "events", e.Group, e.Event, "enable")
+	} else if e.Event != "" {
+		return filepath.Join(i.path, "events", "uprobes", e.Event, "enable")
+	}
+
+	return filepath.Join(i.path, "events", "uprobes", "enable")
+}
+
+func (i *Instance) EnableUprobe(e *UprobeEvent) error {
+	return i.writeFile(i.UprobeEnablePath(e), []byte("1"))
+}
+
+func (i *Instance) DisableUprobe(e *UprobeEvent) error {
+	return i.writeFile(i.UprobeEnablePath(e), []byte("0"))
 }
 
 type FetchArg interface {
